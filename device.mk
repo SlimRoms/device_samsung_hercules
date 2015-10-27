af4873d438843d1b4a8a819768a957fb7c1ff94d
Dan Cartier <nospamdan1@gmail.com>
2015-10-27 06:50:35 +0100

    Hercules: Move some common stuff to celox-common (2/3)
     * Move out kernel common configs
     * Move out partition information
     * Move out device resolution
     * Move out F2FS package call
     * Move out bluetooth firmware file
     * Move out common system props
    
    Credit to Josue Rivera <prbassplayer@gmail.com>
    
    Change-Id: I7d3875b4a7321042b845bed0e72cc128cf4273c3

--- device.mk
+++ device.mk
@@ -24,15 +24,5 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc
 
-# F2FS
-PRODUCT_PACKAGES += \
-    mkfs.f2fs \
-    fsck.f2fs \
-    fibmap.f2fs
-
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
-
 # Inherit from celox-common
 $(call inherit-product, device/samsung/celox-common/celox-common.mk)
